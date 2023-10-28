aae2b8dc7bcfe1a7b447af291718ce8f15222481
Cliff Xuan <cliff.xuan@gmail.com>
2023-10-28 18:15:31 +0100

    cols for widgets

--- src/market_watch/Market_Watch.py
+++ src/market_watch/Market_Watch.py
@@ -5,12 +5,15 @@ from market_watch.utils import set_page_config_once, trading_view
 
 def main():
     st.markdown("# Market Watch")
-    interval = st.radio(
-        "Interval",
-        ["1H", "4H", "1D", "1W", "1M", "3M", "6M", "12M"],
-        horizontal=True,
-        index=3,
-    )
+    chart_cols = st.columns(2)
+    with chart_cols[0]:
+        interval = st.radio(
+            "Interval",
+            ["1H", "4H", "1D", "1W", "1M", "3M", "6M", "12M"],
+            horizontal=True,
+            index=3,
+        )
+        col_cnt = int(st.radio("Columns", [1, 2], index=1, horizontal=True))  # type: ignore
     styles = {
         "Bars": 0,
         "Candles": 1,
@@ -19,27 +22,28 @@ def main():
         "Renko": 4,
         "Point and Figure": 6,
     }
-    style = styles[
-        st.radio(
-            "Bar's Style",
-            styles,
-            index=1,
-            horizontal=True,
-        )
-    ]  # type: ignore
-    cols = st.columns(2)
+    with chart_cols[1]:
+        style = styles[
+            st.radio(
+                "Bar's Style",
+                styles,
+                index=1,
+                horizontal=True,
+            )
+        ]  # type: ignore
+    chart_cols = st.columns(col_cnt)
     kwargs = {
-        "height": 400,
+        "height": 800 // col_cnt,
         "interval": interval,
         "hide_side_toolbar": True,
         "style": style,
     }
-    with cols[0]:
+    with chart_cols[0]:
         trading_view(
             "SPX500USD", "OANDA", **kwargs
         )  # SP:SPX only avail from tradingview
         trading_view("GOLD", "TVC", **kwargs)
-    with cols[1]:
+    with chart_cols[-1]:
         trading_view("DXY", "INDEX", **kwargs)
         trading_view("BLX", "BNC", **kwargs)
 
