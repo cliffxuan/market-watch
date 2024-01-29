de4406e46cb7e19dc7137523989ea5d25cfd4d9e
Cliff Xuan <cliff.xuan@gmail.com>
2024-01-29 21:31:44 +0000

    updated data

--- scripts/trim-git-histroy.sh
+++ scripts/trim-git-histroy.sh
@@ -2,7 +2,7 @@
 # large files are stored in the git repository. use this script to remove them in
 # git change history in order to reduce the git repository size
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "$DIR" || exit 1
+cd "$DIR/.." || exit 1
 
 git filter-branch -f --index-filter 'git rm -rf --cached --ignore-unmatch data/spx_hist.parquet' HEAD
 git for-each-ref --format='delete %(refname)' refs/original | git update-ref --stdin
