7fa5af6a117cf43b454f2e3ca458c84089c419a4
Cliff Xuan <cliff.xuan@gmail.com>
2024-07-19 22:40:42 +0100

    enhanced back testing

--- src/market_watch/pages/7_Back_Testing.py
+++ src/market_watch/pages/7_Back_Testing.py
@@ -4,7 +4,8 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-from market_watch.utils import DATA_DIR, set_page_config_once
+import yfinance as yf
+from market_watch.utils import set_page_config_once
 from plotly.subplots import make_subplots
 
 
@@ -18,24 +19,32 @@ class Transaction(NamedTuple):
 
 
 @st.cache_data(ttl="1h")
-def get_hist(ticker, start=None, end=None):
-    df = pd.read_csv(DATA_DIR / "GOOG.csv")
-    df["Date"] = pd.to_datetime(df["Date"])
+def get_hist(ticker):
+    df = yf.Ticker(ticker).history(period="10y")
+    df["Date"] = df.index.date  # type: ignore
     return df.set_index(["Date"])
-    # return yf.Ticker(ticker).history(start=start, end=end)
 
 
 def main():
-    capital = 10_000
-    fee = 0.002
-    fig = make_subplots(rows=3, cols=1, shared_xaxes=True, row_heights=[0.6, 0.2, 0.2])
-    st.markdown("# Back Testing")
-    hist = get_hist("MSFT", "2004-8-19", "2013-03-01")
+    st.markdown("# Back Testing Trading Strategy")
+    input_cols = st.columns(3)
+    ticker = input_cols[0].selectbox(
+        "yahoo finance ticker", options=["MSFT", "AAPL", "NVDA", "GOOGL"]
+    )
+    capital = input_cols[1].number_input("starting capital", value=10_000)
+    fee = input_cols[2].number_input("fee %", value=0.2) / 100
+    st.markdown("## Moving Average Cross-over")
+    ma_col_0, ma_col_1 = st.columns(2)
+    fast_ma = int(ma_col_0.number_input("Fast Moving Average", value=10))
+    slow_ma = int(ma_col_1.number_input("Slow Moving Average", value=20))
+    hist = get_hist(ticker)
     df = hist.loc[:, ["Close", "Volume"]]
-    df["10SMA"] = hist["Close"].rolling(window=10).mean()
-    df["20SMA"] = hist["Close"].rolling(window=20).mean()
+    fast_ma_name = f"{fast_ma}MA"
+    slow_ma_name = f"{slow_ma}MA"
+    df[fast_ma_name] = hist["Close"].rolling(window=fast_ma).mean()
+    df[slow_ma_name] = hist["Close"].rolling(window=slow_ma).mean()
     df["Price"] = hist["Close"]
-    df["diff"] = df["10SMA"] - df["20SMA"]
+    df["diff"] = df[fast_ma_name] - df[slow_ma_name]
     buy_dates = []
     sell_dates = []
     for d_0, d_1, d_2 in zip(df.index, df.index[1:], df.index[2:]):
@@ -59,7 +68,7 @@ def main():
             sell_price = df.loc[sell_date]["Price"]
             sell_profit = (sell_price - buy_price) / buy_price
             size = np.floor(cash / (buy_price * (1 + fee)))
-            cash -= size * (buy_price + (1 + fee))
+            cash -= size * (buy_price * (1 + fee))
             trades.append(
                 Transaction(buy_date, "Buy", buy_price, buy_profit, size, cash)
             )
@@ -67,16 +76,44 @@ def main():
             trades.append(
                 Transaction(sell_date, "Sell", sell_price, sell_profit, -size, cash)
             )
-    line = px.line(
-        df[["Price", "10SMA", "20SMA"]],
-        width=1024,
-        height=768,
-    )
     trade_df = pd.DataFrame(
         trades, columns=["Date", "TX", "Price", "Profit", "Size", "Cash"]
     ).set_index(["Date"])
+
+    st.markdown("## Trades")
+    result_cols = st.columns(2)
+    with result_cols[0]:
+        st.dataframe(trade_df)
+
+    with result_cols[1]:
+        last_trade = trade_df.iloc[-1]
+        if last_trade["TX"] == "Sell":
+            end_value = last_trade["Cash"]
+        else:
+            end_value = last_trade["Size"] * last_trade["Price"]
+        cols = st.columns(2)
+        cols[0].metric("final capital", f"{end_value:,.0f}")
+        cols[1].metric("profit", f"{end_value / capital * 100:,.2f} %")
+
+        buy_and_hold = (
+            trade_df.iloc[0]["Size"] * df.iloc[-1]["Price"] + trade_df.iloc[0]["Cash"]
+        )
+        cols[0].metric("buy & hold", f"{buy_and_hold:,.0f}")
+        cols[1].metric("buy & hold profit", f"{buy_and_hold / capital * 100:,.2f} %")
+
+        winning_trades = trade_df[trade_df["Profit"] > 0]
+        winning_rate = len(winning_trades) / len(trades)
+        cols[0].metric("total trades", f"{len(trade_df):,}")
+        cols[1].metric("winning rate", f"{winning_rate * 100:,.2f} %")
+
     buy_df = trade_df[trade_df["TX"] == "Buy"]
     sell_df = trade_df[trade_df["TX"] == "Sell"]
+    line = px.line(
+        df[["Price", fast_ma_name, slow_ma_name]],
+        width=1024,
+        height=768,
+    )
+    fig = make_subplots(rows=3, cols=1, shared_xaxes=True, row_heights=[0.6, 0.2, 0.2])
     for trace in line.data:
         fig.append_trace(trace, row=1, col=1)
     fig.append_trace(
@@ -117,8 +154,6 @@ def main():
     )
     fig["layout"].update(width=1024, height=768)
     st.plotly_chart(fig, config={"scrollZoom": True}, use_container_width=True)
-    st.markdown("## Trades")
-    st.dataframe(trade_df)
 
 
 if __name__ == "__main__":
