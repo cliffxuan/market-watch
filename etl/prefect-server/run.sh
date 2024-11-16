609595882b0d8f9a9e5d16ec2683d39738a07d34
Cliff Xuan <cliff.xuan@gmail.com>
2024-11-16 20:58:04 +0000

    Update Apache SSL configuration and run script
    - Added basic authentication to Apache SSL config for restricted access.
    - Updated ProxyPass settings within a <Location> block.
    - Modified run.sh to set PREFECT_API_URL environment variable for Docker container.

--- etl/prefect-server/run.sh
+++ etl/prefect-server/run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$DIR" || exit 1
 docker build -t prefect-server .
-docker run --rm -p 4200:4200 prefect-server
+docker run --rm -p 4200:4200 -e PREFECT_API_URL="https://prefect.nuoya.co.uk/api" prefect-server
