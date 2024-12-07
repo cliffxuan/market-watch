8fc63a96456bd82478377baab07ff2b766a87a57
Cliff Xuan <cliff.xuan@gmail.com>
2024-12-07 23:35:42 +0000

    Refactor Influencers.py: Introduce Video model, improve video processing, and enhance data display

--- src/market_watch/pages/10_Influencers.py
+++ src/market_watch/pages/10_Influencers.py
@@ -1,11 +1,19 @@
+from __future__ import annotations
+
+import datetime as dt
 import json
+import logging
 import textwrap
+from functools import cached_property
 from hashlib import md5
 from pathlib import Path
+from typing import Generator
 
 import openai
+import pandas as pd
 import streamlit as st
 from googleapiclient.discovery import build
+from pydantic import BaseModel, computed_field
 from youtube_transcript_api import YouTubeTranscriptApi
 
 from market_watch.utils import auth_required, set_page_config_once
@@ -22,6 +30,7 @@ CHANNELS = {
     "@VirtualBacon": "UCcrEA_xd9Ldf1C8DIJYdyyA",
     "@Jungernaut": "UCQglaVhGOBI0BR5S6IJnQPg",
 }
+CHANNELS_REVERSE = {v: k for k, v in CHANNELS.items()}
 
 
 PWD = Path(__file__).absolute().parent
@@ -29,20 +38,53 @@ STORE = PWD / ".store"
 STORE.mkdir(exist_ok=True)
 
 
-@st.cache_data(ttl="2h")
-def get_latest_videos(channel_id: str, limit: int = 5) -> list[dict]:
-    return (
-        build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
-        .search()
-        .list(
-            channelId=channel_id,
-            part="id,snippet",
-            order="date",
-            type="video",
-            maxResults=limit,
-        )
-        .execute()
-    ).get("items", [])
+class Video(BaseModel):
+    id: str
+    title: str
+    description: str
+    channel: str
+    publish_time: dt.datetime
+    thumbnail_url: str
+
+    @computed_field
+    @cached_property
+    def captions(self) -> str | None:
+        return get_video_captions(self.id)
+
+    @computed_field
+    @cached_property
+    def summary(self) -> str | None:
+        if self.captions is not None:
+            return (
+                summarize_text("\n".join([self.title, self.description, self.captions]))
+                .replace("$", "\\$")
+                .replace(". ", ".\n")
+            )
+
+    @computed_field
+    def url(self) -> str:
+        return f"https://youtube.com/watch?v={self.id}"
+
+    @computed_field
+    def channel_url(self) -> str:
+        return f"https://youtube.com/{self.channel}"
+
+    @classmethod
+    def process_videos(cls, data: list[dict]) -> Generator[Video, None, None]:
+        for item in data:
+            video = cls.model_validate(
+                {
+                    "id": item["id"]["videoId"],
+                    "title": item["snippet"]["title"],
+                    "description": item["snippet"]["description"],
+                    "thumbnail_url": item["snippet"]["thumbnails"]["default"]["url"],
+                    "channel": CHANNELS_REVERSE[item["snippet"]["channelId"]],
+                    "publish_time": item["snippet"]["publishTime"],
+                }
+            )
+            if video.summary is not None:
+                logging.info("succeed getting summary for vidoe")
+            yield video
 
 
 def get_video_captions(video_id: str) -> str | None:
@@ -70,6 +112,22 @@ def get_video_captions(video_id: str) -> str | None:
     except Exception as e:
         print(f"Could not retrieve captions for {video_id}: {e}")
         return None
+
+
+@st.cache_data(ttl="2h")
+def get_latest_videos(channel_id: str, limit: int = 5) -> list[dict]:
+    return (
+        build("youtube", "v3", developerKey=YOUTUBE_API_KEY)
+        .search()
+        .list(
+            channelId=channel_id,
+            part="id,snippet",
+            order="date",
+            type="video",
+            maxResults=limit,
+        )
+        .execute()
+    ).get("items", [])
 
 
 def summarize_text(text: str, max_length: int = 1_000) -> str:
@@ -132,32 +190,63 @@ def max_tokens_for_summary(text: str) -> int:
 def main() -> None:
     st.markdown("# Influencers")
     # Process each channel
+    all_data = []
+    all_videos = {}
     for channel, channel_id in CHANNELS.items():
-        st.markdown(f"## {channel}")
         with st.spinner(f"\nAnalyzing channel: {channel}"):
-            # Get latest videos
-            videos = get_latest_videos(channel_id, limit=2)
+            data = get_latest_videos(channel_id, limit=2)
+            all_data = [*all_data, *data]
+            for video in Video.process_videos(data):
+                all_videos[video.id] = video
 
-            st.json(videos, expanded=False)
-            for video in videos:
-                video_id = video["id"]["videoId"]
-                title = video["snippet"]["title"]
-                description = video["snippet"]["description"]
-                st.image(video["snippet"]["thumbnails"]["default"]["url"])
-                st.markdown(f"Title: {title}")
-                st.markdown(f"Description: {description}")
-                st.markdown(f"Publish Time: {video['snippet']['publishTime']}")
-                # Get captions
-                captions = get_video_captions(video_id)
-                if captions:
-                    # Summarize captions
-                    summary = summarize_text(
-                        "\n".join([title, description, captions])
-                    ).replace("$", "\\$")
-                    st.markdown(f"Summary: {summary}")
-                else:
-                    st.error("No captions available")
-                st.divider()
+    all_videos_df = pd.DataFrame([video.model_dump() for video in all_videos.values()])
+    all_videos_df["select"] = st.session_state.setdefault(
+        "{__name__}.selected", [False] * len(all_videos)
+    )
+    cols = [
+        "thumbnail_url",
+        "title",
+        "channel_url",
+        "publish_time",
+        "summary",
+    ]
+    selected = st.data_editor(
+        all_videos_df,
+        column_order=["select", *cols],
+        column_config={
+            "select": st.column_config.CheckboxColumn(
+                "",
+                help="Select to see more info and portfolio optimization",
+                default=False,
+            ),
+            "channel_url": st.column_config.LinkColumn(
+                "channel",
+                validate=r"^https://youtube\.com/@(.+)$",
+                max_chars=100,
+                display_text=r"^https://youtube\.com/@(.+)$",
+            ),
+            "thumbnail_url": st.column_config.ImageColumn("thumbnail"),
+        },
+        disabled=cols,
+        hide_index=True,
+        height=(len(all_videos) + 1) * 35 + 3,
+    )
+    ids = list(selected[selected["select"]]["id"])
+    for video_id in ids:
+        video = all_videos[video_id]
+        st.markdown(f"- {video.title}")
+        st.markdown(f"[![{video.url}]({video.thumbnail_url})]({video.url})")
+        st.markdown(f"Channel: [{video.channel}]({video.channel_url})")
+        st.markdown(f"Description: {video.description}")
+        st.markdown(f"Publish Time: {video.publish_time}")
+        if video.captions is not None:
+            st.markdown(f"Summary: {video.summary}")
+        else:
+            st.error("No captions available")
+        st.divider()
+
+    with st.expander("raw data"):
+        st.json(all_data)
 
 
 if __name__ == "__main__":
