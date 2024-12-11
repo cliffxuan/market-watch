d5ba1948eeeadd5a016386982525db02bf597a0c
Cliff Xuan <cliff.xuan@gmail.com>
2024-12-11 21:39:37 +0000

    mount data

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ cd "$DIR" || exit 1
 docker build -t market-watch .
 mkdir -p data
 docker network create warp_network || true
-docker run --rm --network warp_network -v "$DIR/src":/app/src -p 8501:8501 market-watch
+docker run --rm --network warp_network -v "$DIR/src":/app/src -v "$DIR/data":/app/data -p 8501:8501 market-watch
